5ba286c8624883739eb6c4ea871a0621a5bf9a51
Coder of Salvation <info@leon.vankammen.eu>
2013-12-08 16:42:03 +0100

    disabled esx tweak, bugfixed reversedpaddingmode on short samples and improved performance using globbing

--- html/multishotifier.bash
+++ html/multishotifier.bash
@@ -1,9 +1,10 @@
 #!/bin/bash 
 
+collected=0 # global placeholder
+
 _collect(){
-  files=0; dir="$1"; nfiles="$2"; nstartfile="$3"; normalize="$4"; maxlength="$5"; trim="$6"; recursive="$7"; slice="$8"
+  files=0; dir="$1"; nfiles="$2"; nstartfile="$3"; normalize="$4"; maxlength="$5"; trim="$6"; recursive="$7"; slice="$8"; mono="$9"
   [[ ! -d /tmp/.collect ]] && mkdir /tmp/.collect || rm /tmp/.collect/* &>/dev/null
-  [[ ! -d /tmp/.slices  ]] && mkdir /tmp/.slices || rm /tmp/.slices/* &>/dev/null
   (( $normalize == 1 )) && normalize="--norm"
   maxtrim="00:00:00.0 00:10:00.0"
   echo "./collect $dir $nfiles $nstartfile $normalize $maxlength $trim $recursive $slice"
@@ -15,37 +16,39 @@ _collect(){
     wavfile="$(echo "$wavfile" | sed 's/\.\///g')"
     echo "$wavfile"
     samples="$(soxi "$wavfile" | grep Duration | cut -d' ' -f11 )"
-    if [[ ${#samples} > 0 ]] && 
-       (( samples < maxlength )) && (( $files < $nfiles )); then
-      echo "-> processing $wavfile ($samples samples < $maxlength, files/nfiles: $files/$nfiles)"; name="$(basename "$wavfile" | sed 's/WAV/wav/g')"
+    if [[ ${#samples} > 0 ]] && (( $samples < $maxlength )) && (( $collected < $nfiles )); then
+      echo "-> processing $wavfile ($samples samples < $maxlength, files/nfiles: $collected/$nfiles)"; name="$(basename "$wavfile" | sed 's/WAV/wav/g')"
       # if slices is enabled, disable the trim (and set to maxtrim to prevent long waiting times)
       if (( slice > 0 )); then pretrim="$maxtrim"; else pretrim="$trim"; fi
-      sox "$wavfile" $normalize -c 2 -e signed -b 16 -r 44100 "/tmp/.collect/$name.trimmed.wav" trim ${pretrim}
-      if (( slice == 1 )); then 
-        slice "/tmp/.collect/$name.trimmed.wav" /tmp/.slices "$trim" "$nfiles"
-        files=$(ls -1 /tmp/.slices | wc -l )
-      else ((files=files+1)); fi
+      (( $mono == 0 )) && channels="-c 2" || channels="-c 1" 
+      sox "$wavfile" $normalize ${channels} -e signed -b 16 -r 44100 "/tmp/.collect/$name.trimmed.wav" trim ${pretrim}
+      if (( slice == 1 )); then slice "/tmp/.collect/$name.trimmed.wav" /tmp/.collect "$trim" "$nfiles"; else ((collected+=1)); fi
     fi
-    if (( files >= nfiles )); then break; fi 
+    if (( $collected >= nfiles )); then break; fi 
   done < $TMPFILE.filelist
-  echo "collected $files items (wanted=$nfiles)"
+  echo "collected $collected items (wanted=$nfiles)"
 }
 
 # pad sample to length (fills with silence or cuts sample)
 # <input.wav> <00:00:00.00 =00:00:00.00s> <normalize>
 padsample(){
-  inputfile="$1"; trim="$2"; normalize="$3"; 
-  [[ ${#normalize} != 0 ]] && normalize="norm"
-  echo "padding sample $inputfile to $trim"
+  inputfile="$1"; trim="$2"; normalize="$3"; mono="$4"
+  #echo "padding sample $inputfile to $trim"
+  silencefile="/tmp/.collect/multishotsilence.$(echo "$trim" | sed 's/ /-/g').wav"
+  (( $mono == 0 )) && channels="-c 2" || channels="-c 1" 
+  [[ ! -f "$silencefile" ]] && sox -n -e signed -b 16 -r 44100 ${channels} "$silencefile" trim ${trim} 2>&1 # create silence file to enable exact padding
   if [[ ${#padreverse} > 0 ]]; then 
     sox "$inputfile" "$inputfile.paddedreversed.wav" repeat 4 reverse 
-    sox "$inputfile" "$inputfile.paddedreversed.wav" "$inputfile.padded.wav" $normalize trim ${trim} # append & pad wav
-    echo "Reversed!"
+    sox "$inputfile" "$inputfile.paddedreversed.wav" "$inputfile.paddedreversedanddry.wav" 
+    sox -m "$silencefile" "$inputfile.paddedreversedanddry.wav" "$inputfile.padded.wav" trim ${trim}  # pad wav
+    rm "$inputfile.paddedreversedanddry.wav"
   else
-    silencefile="/tmp/.collect/multishotsilence.$(echo "$trim" | sed 's/ /-/g').wav"
-    [[ ! -f "$silencefile" ]] && sox -n -e signed -b 16 -r 44100 -c 2 "$silencefile" trim ${trim} 2>&1 # create silence file to enable exact padding
-    sox -m "$silencefile" "$inputfile" "$inputfile.padded.wav" $normalize # pad wav
+    sox -m "$silencefile" "$inputfile" "$inputfile.padded.wav" trim ${trim}  # pad wav
   fi
+  if [[ ${#normalize} != 0 ]]; then 
+    sox "$inputfile.padded.wav" "$inputfile.paddednorm.wav" norm 
+    mv "$inputfile.paddednorm.wav" "$inputfile.padded.wav"
+  fi 
 }
 
 # bundles all .wav files in a dir into one file, makes it mono, pitches up, does custom stuff, pads samples with silence, normalizes
@@ -54,26 +57,27 @@ _bundle(){
   indir="$1"; outfile="$2"; nfiles="$3"; mono="$4"; pitchup="$5"; soxextra="${6}"; trim="$7"; normalize="$8"; 
   esxoptimize="$9"; 
   echo "_bundle $indir $outfile $nfiles $mono $pitchup $soxextra $trim $normalize $esxoptimize"
-  (( mono == 1 )) && monoarg="-c 1"
+  (( $mono == 0 )) && channels="-c 2" && channels="-c 1"
   cd "$indir"
   rm *.padded.wav &>/dev/null
-  ls *.wav | while read file; do padsample "$file" "$trim" "$normalize"; done
+  ls *.wav | while read file; do padsample "$file" "$trim" "$normalize" "$mono"; done
   if ls *.padded.wav &>/dev/null; then 
-	  #files="$(ls *.padded.wav | head -n$nfiles )"
     files=( *.padded.wav );
-    echo sox ${files[@]:1:$nfiles} ${monoarg} $outfile speed "$pitchup" 
-    sox "${files[@]:0:$nfiles}" ${monoarg} $outfile speed "$pitchup" 
+    files=("${files[@]:0:$nfiles}") # create subset of nfiles length
+    echo sox ${files[@]} ${channels} $outfile speed "$pitchup" 
+    sox "${files[@]}" ${channels} $outfile speed "$pitchup" 
     extra="$( printf "$soxextra" "$outfile" "$outfile.wav")"; echo "$extra";
     ${extra}; mv "$outfile.wav" "$outfile" &>/dev/null
-    echo "written $(echo "$files" | wc -l) samples to $outfile ($(stat -c%s "$outfile") bytes)"
+    soxi "$outfile"
+    echo "written ${#files[@]} samples to $outfile ($(stat -c%s "$outfile") bytes)"
   else echo "no wavfiles found to glue to output file"; fi
+  (( ${#files[@]} != $nfiles )) && echo "WARNING: bundled ${#files[@]} instead of $nfiles files..alignment will not be perfect"
   if (( $esxoptimize > 0 )); then optimizeESX "$outfile" "$pitchup" "$mono"; fi
   rm *.padded.wav
 }
 
 optimizeESX(){
   file="$1"; pitch="$2"; mono="$3"; padding=1250;
-  (( $mono == 0 )) && ((padding=padding*2))
   padding=$( echo "$padding*$pitch" | bc | xargs printf "%1.0f" );
   echo "esxoptimize: trimming $padding samples to ensure proper startposition-snap"
   sox "$file" "$file.wav" trim "$padding"s && mv "$file.wav" "$file"
@@ -89,6 +93,7 @@ slice(){
   echo "$hits" > $TMPFILE.hits
   declare -a hitArray; last=""; i=0
   while read -r line; do hitArray[$i]="$line"; ((i++)); done < $TMPFILE.hits
+  [[ ${#hitArray} == 0 ]] && echo "no slices found" && ((collected+=1))
   for((i=0;i<${#hitArray[@]}-1;i++)); do
     hit=${hitArray[$i]}
     hitnext=${hitArray[$i+1]}
@@ -98,7 +103,8 @@ slice(){
     outfile="$outdir/$(basename "$input.slice-$i.wav" )"
     echo "writing $outfile slice #$i"
     sox "$input" "$outfile" trim $hit $length trim ${trim} # fade t 0 $length 0.02
-    (( i >= maxslices )) && return 0
+    ((collected++))
+    (( $collected >= maxslices )) && return 0
   done
 }
 
@@ -120,9 +126,9 @@ _writemultishot(){
              "$sampleframes"  \
              "$sampletrim"    \
              "$_recursive"    \
-             "$_slice" 
-      if (( $slice == 1 )); then indir="/tmp/.slices"; else indir="/tmp/.collect"; fi
-      _bundle  "$indir"        \
+             "$_slice"        \
+             "$_mono" 
+      _bundle  "/tmp/.collect" \
                "$_outfile"     \
                "$nfiles"       \
                "$_mono"        \
